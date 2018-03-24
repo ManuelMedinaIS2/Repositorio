ea014d2bfeacd61cb29b506d90fdc9a1a9193126
ManuelMedinaIS2 <manuelmedina012@gmail.com>
2018-03-24 03:51:41 -0300

    documentacion!!!!! githinx

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 #from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns= [
+urlpatterns= {
     url(r'^admin/', admin.site.urls),
-]
+}
 
 
 
